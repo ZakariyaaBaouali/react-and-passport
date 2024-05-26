aed7c9909711caf23b72a0a3facef7cfdf4fd60f
zakaria <zakariabaouali@outlook.com>
2024-05-26 15:14:24 +0100

    Init setup commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const App = ()  => {
+  return (
+    <Container>App</Container>
+  )
+}
+
+export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(to right , #13aeec , #d108f0);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
